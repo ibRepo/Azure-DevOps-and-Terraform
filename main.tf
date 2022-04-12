4291ea3b2d7bb1264addb991c7df0a26de970444
Igor <igor.bernard87@gmail.com>
2022-04-12 12:10:59 +0200

    Updated main.tf file with var

--- main.tf
+++ main.tf
@@ -3,6 +3,20 @@ provider "azurerm" {
     features {}
 }
 
+terraform {
+    backend "azurerm" {
+            resource_group_name     = "tf_rg_blobstore"
+            storage_account_name    = "tfstoragedock2master"
+            container_name          = "tfstate"
+            key                     = "terraform.tfstate"
+    }
+}
+
+variable "imagebuild" {
+  type        = string
+  description = "Latest Image Build"
+}
+
 resource "azurerm_resource_group" "tf_test" {
     name = "tfmainrg"
     location = "West Europe"
@@ -19,7 +33,7 @@ resource "azurerm_container_group" "tfcg_test" {
 
     container {
         name    = "weatherapi"
-        image   = dock2master/weatherapi
+        image   = "dock2master/weatherapi:${var.imagebuild}"
         cpu     = "1"
         memory  = "1"
 
